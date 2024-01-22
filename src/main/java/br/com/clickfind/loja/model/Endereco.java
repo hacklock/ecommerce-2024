5408a8a6a35035ea618d35b03e88e6c1bd466c74
Wilton R. Cruz <jiquitaiasoftware@gmail.com>
2024-01-22 10:54:26 -0300

    Criando as classes Conta/Pagar/Receber/Enum/etc

--- src/main/java/br/com/clickfind/loja/model/Endereco.java
+++ src/main/java/br/com/clickfind/loja/model/Endereco.java
@@ -1,7 +1,6 @@
 package br.com.clickfind.loja.model;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import br.com.clickfind.loja.enums.TipoEndereco;
 import jakarta.persistence.ConstraintMode;
@@ -27,6 +26,7 @@ public class Endereco implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_endereco")
 	private Long id;
+
 	private String ruaLogra;
 	private String cep;
 	private String numero;
@@ -38,16 +38,17 @@ public class Endereco implements Serializable {
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
-
+	
 	@Enumerated(EnumType.STRING)
 	private TipoEndereco tipoEndereco;
-
-	public TipoEndereco getTipoEndereco() {
-		return tipoEndereco;
-	}
-
+	
+	
 	public void setTipoEndereco(TipoEndereco tipoEndereco) {
 		this.tipoEndereco = tipoEndereco;
+	}
+	
+	public TipoEndereco getTipoEndereco() {
+		return tipoEndereco;
 	}
 
 	public Long getId() {
@@ -124,7 +125,10 @@ public class Endereco implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -136,7 +140,12 @@ public class Endereco implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Endereco other = (Endereco) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 }
